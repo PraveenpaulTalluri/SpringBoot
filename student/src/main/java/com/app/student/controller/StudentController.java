5b5260f161ba65686a0f645edbf876fec557d9e5
Praveen Talluri <talluri_p@India.XoriantCorp.com>
2021-11-18 18:51:22 +0530

    Exception and Logging

--- student/src/main/java/com/app/student/controller/StudentController.java
+++ student/src/main/java/com/app/student/controller/StudentController.java
@@ -2,24 +2,47 @@ package com.app.student.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.app.student.model.Student;
 import com.app.student.service.StudentService;
 
 @RestController
 @RequestMapping("/restApi")
 public class StudentController {
+	
+
+	private static final Logger log = LoggerFactory.getLogger(StudentController.class);
+	
 	@Autowired
 	StudentService studentservice;
 	
-	@GetMapping
+	@GetMapping("/students")
 	public ResponseEntity<List<Student>> getAllStudents()
 	{
 		return ResponseEntity.ok().body(studentservice.getAllStudents());
 	}
+	
+	@GetMapping("/orders/{admission_id}")
+	public ResponseEntity<Student> getStudentByAdmissionId(@PathVariable(value = "admission_id") Integer admission_id)
+	 {
+		Student studentObj= null;
+		log.info("Student admissionId is "+admission_id);
+		studentObj= studentservice.findStudentByAdmissionId(admission_id);
+		if(studentObj!=null) {
+			return ResponseEntity.ok().body(studentObj);
+		}else {
+			return new ResponseEntity<Student>(studentObj, HttpStatus.NOT_FOUND);
+		}
+		
+	}
+	
+	
 }
--- student/src/main/java/com/app/student/service/StudentService.java
+++ student/src/main/java/com/app/student/service/StudentService.java
@@ -1,19 +1,38 @@
 package com.app.student.service;
 
 import java.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.api.student.exception.ResourceNotFoundException;
+import com.app.student.controller.StudentController;
 import com.app.student.model.Student;
 import com.app.student.repository.StudentRepository;
 
 @Service
 public class StudentService {
+	
+
+	private static final Logger log = LoggerFactory.getLogger(StudentController.class);
 	@Autowired
 	private StudentRepository studentrepository;
 
 	public List<Student> getAllStudents() 
 	{
 		return studentrepository.findAll();
+	}
+	
+	public Student findStudentByAdmissionId(Integer admission_id)
+	{
+		
+		Student student=null;
+		try {
+			student = studentrepository.findById(admission_id).orElseThrow(() -> new ResourceNotFoundException("student not found for this id :: " + admission_id));
+		} catch (ResourceNotFoundException e) {
+			log.error("Error while fetching Student for admissionId "+admission_id+ " :: " +e.getMessage());
+		}
+		return student;
 	}
 }
